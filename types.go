7eb4322ec24d69fd8e218ff483fd7906b85058f3
morganc3 <me@clbymrgn.com>
2020-08-17 20:23:23 -0500

    .

--- types.go
+++ types.go
@@ -273,7 +273,7 @@ func (rr *CNAME) String() string { return rr.Hdr.String() + sprintName(rr.Target
 
 type ALIAS struct {
 	Hdr    RR_Header
-	Target string `dns:"domain-name"`
+	Target string `dns:"cdomain-name"`
 }
 
 func (rr *ALIAS) String() string {
